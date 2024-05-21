e4984d6548068ed103760acdfea33eea39d7f4df
SUMANTH161 <93914950+SUMANTH161@users.noreply.github.com>
2024-05-21 19:15:55 +0530

    Update a1.java

--- a1.java
+++ a1.java
@@ -1,7 +1,7 @@
 // Online Java Compiler
 // Use this editor to write, compile and run your Java code online
 
-class main {
+class Main {
     public static void main(String[] args) {
         System.out.println("Try programiz.pro");
     }
